ce28f766192bca5a950302f526c39ac6949d1e95
Cozyamy <cozyamy@gmail.com>
2023-01-04 01:03:44 +0100

    Alx interview lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
-""" Method that determines if all the boxes can be opened """
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes
+More info:
+- Prototype: def canUnlockAll(boxes)
+- boxes is a list of lists
+- A key with the same number as a box opens that box
+- You can assume all keys will be positive integers
+- The first box boxes[0] is unlocked
+- Return True if all boxes can be opened, else return False
+"""
 
 
 def canUnlockAll(boxes):
-    """ Determines if all the boxes can be opened"""
-    size = len(boxes)
-    checker = {}
-    index = 0
-
-    for keys in boxes:
-        if len(keys) == 0 or index == 0:
-            checker[index] = -1  # -1 means box is empty
-        for key in keys:
-                if key < size and key != index:
-                    checker[key] = key
-        if len(checker) == size:
-            return True
-        index += 1
+    """
+    Description:
+    Write a method that determines if all the boxes can be opened
+    Arguments:
+    boxes --> List of Lists, it contains the boxes with keys
+    Reurn boolean
+    Variables:
+    myKeys --> List, Store the number keys to open boxes
+    key --> integer, key of the myKeys
+    boxKey --> iinteger, key inside of an specific box
+    """
+    myKeys = [0]
+    for key in myKeys:
+        for boxKey in boxes[key]:
+            if boxKey not in myKeys and boxKey < len(boxes):
+                myKeys.append(boxKey)
+    if len(myKeys) == len(boxes):
+        return True
     return False
